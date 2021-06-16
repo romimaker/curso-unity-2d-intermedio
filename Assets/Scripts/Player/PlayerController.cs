cfc27205cbc81411ae6ddef40f07522b7bf0a10d
Daniel Sanchez <jdanielsancheza@gmail.com>
2021-06-16 15:27:12 -0500

    New Input Systeam

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D rb;
+
+    private float movementX;
+    private float movementY;
+
+    public int speed;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnMove(InputValue movementValue)
+    {
+        Vector2 movementVector = movementValue.Get<Vector2>();
+        movementX = movementVector.x;
+        movementY = movementVector.y;
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(movementX, movementY) * speed;
+    }
+}
