55ac576c4fc9a6d1484d3456c6fd246e2110ca49
Daniel Sanchez <jdanielsancheza@gmail.com>
2021-06-16 17:54:30 -0500

    DoTween

---
+++ Assets/Scripts/Enemy/ShipMovement.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class ShipMovement : MonoBehaviour
+{
+    private void Start()
+    {
+        transform.DOMoveX(0,2);
+    }
+}
