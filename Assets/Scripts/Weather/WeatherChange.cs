4853bc004f3736e0c092ff9bccfc5ff0ce0f9e42
Daniel Sanchez <jdanielsancheza@gmail.com>
2021-06-17 14:54:49 -0500

    WebRequest

---
+++ Assets/Scripts/Weather/WeatherChange.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class WeatherChange : MonoBehaviour
+{
+    void Start()
+    {
+        StartCoroutine(GetWeather());
+    }
+
+    IEnumerator GetWeather()
+    {
+        UnityWebRequest www = UnityWebRequest.Get("api.openweathermap.org/data/2.5/weather?q=bogota&appid=9fc8072b0267bce57438774724d0f689");
+        yield return www.SendWebRequest();
+
+        if(www.result != UnityWebRequest.Result.Success)
+        {
+            Debug.LogError(www.error);
+        }
+        else {
+            Debug.Log(www.downloadHandler.text);
+        }
+    }
+}
