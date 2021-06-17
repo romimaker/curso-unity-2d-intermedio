0169d878986ca3fb9ff0f23094eed443ed7d8183
Daniel Sanchez <jdanielsancheza@gmail.com>
2021-06-17 15:48:00 -0500

    Weatherchanger

--- Assets/Scripts/Weather/WeatherChange.cs
+++ Assets/Scripts/Weather/WeatherChange.cs
@@ -2,12 +2,54 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
+using LitJson;
 
 public class WeatherChange : MonoBehaviour
 {
+    private int actualWeather;
+    [SerializeField] DigitalRuby.RainMaker.RainScript2D rainMaker;
     void Start()
     {
         StartCoroutine(GetWeather());
+    }
+
+    private void WeatherChanger()
+    {
+        if (actualWeather >= 200 && actualWeather < 300)
+        {
+            //tormenta
+            rainMaker.GetComponent<DigitalRuby.RainMaker.RainScript2D>().RainIntensity += 1;
+        }
+        else if (actualWeather >= 300 && actualWeather < 400)
+        {
+            //llovizna
+            rainMaker.GetComponent<DigitalRuby.RainMaker.RainScript2D>().RainIntensity += 0.2f;
+        }
+        else if (actualWeather >= 400 && actualWeather < 500)
+        {
+            //lluvia
+            rainMaker.GetComponent<DigitalRuby.RainMaker.RainScript2D>().RainIntensity += 0.55f;
+        }
+        else if (actualWeather >= 500 && actualWeather < 600)
+        {
+            //lluvia
+            rainMaker.GetComponent<DigitalRuby.RainMaker.RainScript2D>().RainIntensity += 0.7f;
+        }
+        else if (actualWeather >= 700 && actualWeather < 800)
+        {
+            //niebla
+            rainMaker.RainIntensity += 0.1f;
+        }
+        else if (actualWeather > 800)
+        {
+            //Nubes
+            rainMaker.RainIntensity += 0.1f;
+        }
+        else if (actualWeather == 800)
+        {
+            rainMaker.gameObject.SetActive(false);
+            //ClearSky
+        }
     }
 
     IEnumerator GetWeather()
@@ -18,9 +60,15 @@ public class WeatherChange : MonoBehaviour
         if(www.result != UnityWebRequest.Result.Success)
         {
             Debug.LogError(www.error);
+            actualWeather = 800;
         }
         else {
-            Debug.Log(www.downloadHandler.text);
+            JsonData jsonData = JsonMapper.ToObject(www.downloadHandler.text);
+            actualWeather = (int)jsonData["weather"][0]["id"];
         }
+
+        Debug.Log(actualWeather);
+        WeatherChanger();
+        StopCoroutine(GetWeather());
     }
 }
