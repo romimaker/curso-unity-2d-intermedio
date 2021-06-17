67a535c78f84298cf68896d4b50fd9dd2835b888
Daniel Sanchez <jdanielsancheza@gmail.com>
2021-06-17 14:20:18 -0500

    Callbacks

--- Assets/Scripts/transiction/Fade.cs
+++ Assets/Scripts/transiction/Fade.cs
@@ -11,13 +11,22 @@ public class Fade : MonoBehaviour
     [ContextMenu("FadeIn")]
     public void FadeIn()
     {
-        spriteRenderer.DOFade(1,2);
+        spriteRenderer.DOFade(1,2).OnComplete(()=> {
+            Debug.Log("FadeIn Completo");
+        });
     }
     
     [ContextMenu("FadeOut")]
     public void FadeOut()
     {
-        spriteRenderer.DOFade(0,2);
+        spriteRenderer.DOFade(0,2).OnComplete(()=>StartGame()).OnStart(()=>{
+            Debug.Log("FadeOut iniciado");
+        });
+    }
+
+    private void StartGame()
+    {
+        Debug.Log("Fade Out completo");
     }
 
     private void Start()
